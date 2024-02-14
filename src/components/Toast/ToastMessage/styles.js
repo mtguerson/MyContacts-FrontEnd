4a688a2a41228439eea7f3f83c8b947dc21ebdc8
Matheus Guerson <mtguerson@gmail.com>
2024-02-14 15:34:45 -0400

    adding variants in styled components

--- src/components/Toast/ToastMessage/styles.js
+++ src/components/Toast/ToastMessage/styles.js
@@ -1,4 +1,16 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const containerVariants = {
+  default: css`
+    background: ${({ theme }) => theme.colors.primary.main};
+  `,
+  success: css`
+    background: ${({ theme }) => theme.colors.success.main};
+  `,
+  danger: css`
+    background: ${({ theme }) => theme.colors.danger.main};
+  `,
+};
 
 export const Container = styled.div`
   padding: 16px 32px;
@@ -9,6 +21,8 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  ${({ type }) => containerVariants[type] || containerVariants.default}
 
   & + & {
     margin-top: 12px;
